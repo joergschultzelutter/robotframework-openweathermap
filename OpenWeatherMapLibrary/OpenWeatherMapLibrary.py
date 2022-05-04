5732645227ab416c5c5b9ae051e1d776fa02dddc
Joerg Schultze-Lutter <joerg.schultze.lutter@gmail.com>
2022-05-04 20:22:06 +0200

    Keyword Tests

--- OpenWeatherMapLibrary/OpenWeatherMapLibrary.py
+++ OpenWeatherMapLibrary/OpenWeatherMapLibrary.py
@@ -45,9 +45,9 @@ class OpenWeatherMapLibrary:
     DEFAULT_LATITUDE = 0.0
     DEFAULT_LONGITUDE = 0.0
     DEFAULT_APIKEY = None
-    DEFAULT_FORMAT = "json"
-    DEFAULT_UNIT = "standard"
-    DEFAULT_LANG = None
+    DEFAULT_OUTPUT_FORMAT = "json"
+    DEFAULT_UNIT_FORMAT = "standard"
+    DEFAULT_LANGUAGE = None
     DEFAULT_EXCLUDE = None
     DEFAULT_NUMBER_OF_RESULTS = None
 
@@ -55,31 +55,31 @@ class OpenWeatherMapLibrary:
     __owm_latitude = None
     __owm_longitude = None
     __owm_apikey = None
-    __owm_format = None
-    __owm_unit = None
-    __owm_lang = None
+    __owm_output_format = None
+    __owm_unit_format = None
+    __owm_language = None
     __owm_exclude = None
     __owm_number = None
 
     def __init__(
-            self,
-            owm_latitude: float = DEFAULT_LATITUDE,
-            owm_longitude: float = DEFAULT_LONGITUDE,
-            owm_apikey: str = DEFAULT_APIKEY,
-            own_format: str = DEFAULT_FORMAT,
-            owm_unit: str = DEFAULT_UNIT,
-            owm_lang: str = DEFAULT_LANG,
-            owm_exclude: str = DEFAULT_EXCLUDE,
-            owm_number: int = DEFAULT_NUMBER_OF_RESULTS,
+        self,
+        owm_latitude: float = DEFAULT_LATITUDE,
+        owm_longitude: float = DEFAULT_LONGITUDE,
+        owm_apikey: str = DEFAULT_APIKEY,
+        own_output_format: str = DEFAULT_OUTPUT_FORMAT,
+        owm_unit_format: str = DEFAULT_UNIT_FORMAT,
+        owm_language: str = DEFAULT_LANGUAGE,
+        owm_exclude: str = DEFAULT_EXCLUDE,
+        owm_number: int = DEFAULT_NUMBER_OF_RESULTS,
     ):
         self.__owm_latitude = owm_latitude
         self.__owm_longitude = owm_longitude
         self.__owm_apikey = owm_apikey
         self.__owm_number = owm_number
-        self.__owm_lang = owm_lang
+        self.__owm_language = owm_language
         self.__owm_exclude = owm_exclude
-        self.__owm_format = own_format
-        self.__owm_unit = owm_unit
+        self.__owm_output_format = own_output_format
+        self.__owm_unit_format = owm_unit_format
 
     # Python "Getter" methods
     #
@@ -103,20 +103,20 @@ class OpenWeatherMapLibrary:
         return self.__owm_number
 
     @property
-    def owm_lang(self):
-        return self.__owm_lang
+    def owm_language(self):
+        return self.__owm_language
 
     @property
     def owm_exclude(self):
         return self.__owm_exclude
 
     @property
-    def owm_format(self):
-        return self.__owm_format
+    def owm_output_format(self):
+        return self.__owm_output_format
 
     @property
-    def owm_unit(self):
-        return self.__owm_unit
+    def owm_unit_format(self):
+        return self.__owm_unit_format
 
     # Python "Setter" methods
     #
@@ -146,31 +146,81 @@ class OpenWeatherMapLibrary:
     def owm_number(self, owm_number: int):
         if not owm_number:
             raise ValueError("No Number of Results value has been specified")
+        if owm_number < 1:
+            raise ValueError("Number of Results value needs to be greater than zero")
         self.__owm_number = owm_number
 
-    @owm_lang.setter
-    def owm_lang(self, owm_lang: str):
-        if not owm_lang:
-            raise ValueError("No lang value has been specified")
-        self.__owm_lang = owm_lang
+    @owm_language.setter
+    def owm_language(self, owm_language: str):
+        # fmt: off
+        valid_languages = [
+            "af", "al", "ar", "az",
+            "bg", "ca", "cz", "da",
+            "de", "el", "en", "eu",
+            "fa", "fi", "fr", "gl",
+            "he", "hi", "hr", "hu",
+            "id", "it", "ja", "kr",
+            "la", "lt", "mk", "no",
+            "nl", "pl", "pt", "ro",
+            "ru", "sv", "se", "sk",
+            "sl", "sp", "es", "sr",
+            "th", "tr", "ua", "uk",
+            "vi", "cn", "tw", "zu",
+        ]
+        # fmt: on
+
+        if not owm_language:
+            raise ValueError("No language value has been specified")
+        owm_language = owm_language.lower()
+        if owm_language not in valid_languages:
+            raise ValueError(
+                f"Invalid language code specified; valid values: {valid_languages}"
+            )
+
+        if owm_language == "cn":
+            owm_language = "zh_cn"
+        if owm_language == "tw":
+            owm_language = "zh_tw"
+
+        self.__owm_language = owm_language
 
     @owm_exclude.setter
     def owm_exclude(self, owm_exclude: str):
+        valid_excludes = ["current", "minutely", "hourly", "daily", "alerts"]
         if not owm_exclude:
             raise ValueError("No exclude value has been specified")
+        owm_exclude = owm_exclude.lower()
+        excludes = owm_exclude.split(",")
+        for exclude in excludes:
+            if exclude not in valid_excludes:
+                raise ValueError(
+                    f"Invalid output format specified; valid values: {valid_excludes}"
+                )
         self.__owm_exclude = owm_exclude
 
-    @owm_format.setter
-    def owm_format(self, owm_format: str):
-        if not owm_format:
+    @owm_output_format.setter
+    def owm_output_format(self, owm_output_format: str):
+        valid_formats = ["xml", "html", "json"]
+        if not owm_output_format:
             raise ValueError("No output format value has been specified")
-        self.__owm_format = owm_format
+        owm_output_format = owm_output_format.lower()
+        if owm_output_format not in valid_formats:
+            raise ValueError(
+                f"Invalid output format specified; valid values: {valid_formats}"
+            )
+        self.__owm_output_format = owm_output_format
 
-    @owm_unit.setter
-    def owm_unit(self, owm_unit: str):
-        if not owm_unit:
+    @owm_unit_format.setter
+    def owm_unit_format(self, owm_unit_format: str):
+        valid_units = ["standard", "metric", "imperial"]
+        if not owm_unit_format:
             raise ValueError("No unit of measure value has been specified")
-        self.__owm_unit = owm_unit
+        owm_unit_format = owm_unit_format.lower()
+        if owm_unit_format not in valid_units:
+            raise ValueError(
+                f"Invalid unit format specified; valid values: {valid_units}"
+            )
+        self.__owm_unit_format = owm_unit_format
 
     #
     # Robot-specific "getter" keywords
@@ -192,20 +242,20 @@ class OpenWeatherMapLibrary:
         return self.owm_number
 
     @keyword("Get OpenWeatherMap Language")
-    def get_owm_lang(self):
-        return self.owm_lang
+    def get_owm_language(self):
+        return self.owm_language
 
     @keyword("Get OpenWeatherMap Excludes")
     def get_owm_exclude(self):
         return self.owm_exclude
 
     @keyword("Get OpenWeatherMap Output Format")
-    def get_owm_format(self):
-        return self.owm_format
+    def get_owm_output_format(self):
+        return self.owm_output_format
 
     @keyword("Get OpenWeatherMap Unit Format")
-    def get_owm_unit(self):
-        return self.owm_unit
+    def get_owm_unit_format(self):
+        return self.owm_unit_format
 
     #
     # Robot-specific "setter" keywords
@@ -231,9 +281,9 @@ class OpenWeatherMapLibrary:
         self.owm_number = owm_number
 
     @keyword("Set OpenWeatherMap Language")
-    def set_owm_lang(self, owm_lang: str = None):
+    def set_owm_language(self, owm_language: str = None):
         logger.debug(msg="Setting OWM Language")
-        self.owm_lang = owm_lang
+        self.owm_language = owm_language
 
     @keyword("Set OpenWeatherMap Excludes")
     def set_owm_excludes(self, owm_exclude: str = None):
@@ -241,60 +291,111 @@ class OpenWeatherMapLibrary:
         self.owm_exclude = owm_exclude
 
     @keyword("Set OpenWeatherMap Output Format")
-    def set_owm_lang(self, owm_format: str = None):
+    def set_owm_output_format(self, owm_output_format: str = None):
         logger.debug(msg="Setting OWM Output Format")
-        self.owm_format = owm_format
+        self.owm_output_format = owm_output_format
 
     @keyword("Set OpenWeatherMap Unit Format")
-    def set_owm_unit(self, owm_unit: str = None):
-        logger.debug(msg="Setting OWM Unit")
-        self.owm_unit = owm_unit
+    def set_owm_unit_format(self, owm_unit_format: str = None):
+        logger.debug(msg="Setting OWM Unit Format")
+        self.owm_unit_format = owm_unit_format
 
     #
     # Robot Framework Action Keywords for OpenWeatherMap
     #
     @keyword("Get Current Weather")
-    def get_current_weather(self, latitude: float = None, longitude: float = None, apikey: float = None):
+    def get_current_weather(
+        self, latitude: float = None, longitude: float = None, apikey: float = None
+    ):
         __pro_api = False
         pass
 
     @keyword("Get Hourly Forecasts Four Days")
-    def get_hourly_forecasts_four_days(self,latitude: float = None, longitude: float = None, apikey: float = None, format: str = None, number: int = None, lang: str=None):
+    def get_hourly_forecasts_four_days(
+        self,
+        latitude: float = None,
+        longitude: float = None,
+        apikey: float = None,
+        output_format: str = None,
+        number: int = None,
+        language: str = None,
+    ):
         __pro_api = True
         pass
 
     @keyword("Get OneCall Forecast")
-    def get_onecall_forecast(self,latitude: float = None, longitude: float = None, apikey: float = None, exclude: str = None, unit: str = None, lang: str=None):
+    def get_onecall_forecast(
+        self,
+        latitude: float = None,
+        longitude: float = None,
+        apikey: float = None,
+        exclude: str = None,
+        unit_format: str = None,
+        language: str = None,
+    ):
         __pro_api = False
         pass
 
     @keyword("Get Daily Forecasts 16 Days")
-    def get_daily_forecasts_16_days(self,latitude: float = None, longitude: float = None, apikey: float = None, number: int = None, format: str = None, unit: str = None, lang: str=None):
+    def get_daily_forecasts_16_days(
+        self,
+        latitude: float = None,
+        longitude: float = None,
+        apikey: float = None,
+        number: int = None,
+        output_format: str = None,
+        unit_format: str = None,
+        language: str = None,
+    ):
         __pro_api = False
         pass
 
     @keyword("Get Climatic Forecast 30 Days")
-    def get_climatic_forecast_30_days(self, latitude: float = None, longitude: float = None, apikey: float = None, number: int = None, format: str = None, unit: str = None, lang: str=None):
+    def get_climatic_forecast_30_days(
+        self,
+        latitude: float = None,
+        longitude: float = None,
+        apikey: float = None,
+        number: int = None,
+        output_format: str = None,
+        unit_format: str = None,
+        language: str = None,
+    ):
         __pro_api = True
         pass
 
     @keyword("Get Current Solar Radiation")
-    def get_current_solar_radiation(self, latitude: float = None, longitude: float = None, apikey: float = None):
+    def get_current_solar_radiation(
+        self, latitude: float = None, longitude: float = None, apikey: float = None
+    ):
         __pro_api = False
         pass
 
     @keyword("Get Solar Radiation Forecast")
-    def get_solar_radiation_forecast(self, latitude: float = None, longitude: float = None, apikey: float = None):
+    def get_solar_radiation_forecast(
+        self, latitude: float = None, longitude: float = None, apikey: float = None
+    ):
         __pro_api = False
         pass
 
     @keyword("Get 5 Day 3 Hour Forecast")
-    def get_5_day_3_hour_forecast(self, latitude: float = None, longitude: float = None, apikey: float = None, number: int = None, format: str = None, unit: str = None, lang: str=None):
+    def get_5_day_3_hour_forecast(
+        self,
+        latitude: float = None,
+        longitude: float = None,
+        apikey: float = None,
+        number: int = None,
+        output_format: str = None,
+        unit_format: str = None,
+        language: str = None,
+    ):
         __pro_api = False
         pass
 
     @keyword("Get Air Pollution Data")
-    def get_air_pollution_data(self, latitude: float = None, longitude: float = None, apikey: float = None):
+    def get_air_pollution_data(
+        self, latitude: float = None, longitude: float = None, apikey: float = None
+    ):
         __pro_api = False
         pass
 
---
+++ OpenWeatherMapLibrary/library_checks.robot
@@ -0,0 +1,59 @@
+*** Settings ***
+Library						OpenWeatherMapLibrary.py
+
+*** Variables ***
+
+*** Test Cases ***
+Latitude Getter Setter
+	${VALUE1}=			Convert To Number	1.234	
+	Set OpenWeatherMap Latitude	${VALUE1}	
+	${VALUE2}=			Get OpenWeatherMap Latitude
+	Should Be Equal			${VALUE1}	${VALUE2}
+
+Longitude Getter Setter
+	${VALUE1}=			Convert To Number	5.678	
+	Set OpenWeatherMap Longitude 	${VALUE1}	
+	${VALUE2}=			Get OpenWeatherMap Longitude
+	Should Be Equal			${VALUE1}	${VALUE2}
+
+API Key Getter Setter
+	Set Local Variable		${VALUE1}	myAPIKey
+	Set OpenWeatherMap API Key 	${VALUE1}	
+	${VALUE2}=			Get OpenWeatherMap API Key
+	Should Be Equal			${VALUE1}	${VALUE2}
+
+Number Of Results Getter Setter
+	${VALUE1}=			Convert To Integer	1234
+	Set OpenWeatherMap Number Of Results	${VALUE1}	
+	${VALUE2}=			Get OpenWeatherMap Number Of Results
+	Should Be Equal			${VALUE1}	${VALUE2}
+
+Excludes Getter Setter
+	Set Local Variable		${VALUE1}	hourly
+	Set OpenWeatherMap Excludes 	${VALUE1}	
+	${VALUE2}=			Get OpenWeatherMap Excludes
+	Should Be Equal			${VALUE1}	${VALUE2}
+
+Output Format Getter Setter
+	Set Local Variable		${VALUE1}	json
+	Set OpenWeatherMap Output Format	${VALUE1}	
+	${VALUE2}=			Get OpenWeatherMap Output Format
+	Should Be Equal			${VALUE1}	${VALUE2}
+
+Unit Format Getter Setter
+	Set Local Variable		${VALUE1}	metric	
+	Set OpenWeatherMap Unit Format	${VALUE1}	
+	${VALUE2}=			Get OpenWeatherMap Unit Format
+	Should Be Equal			${VALUE1}	${VALUE2}
+
+Language Getter Setter
+	Set Local Variable		${VALUE1}	de	
+	Set OpenWeatherMap Language	${VALUE1}	
+	${VALUE2}=			Get OpenWeatherMap Language
+	Should Be Equal			${VALUE1}	${VALUE2}
+
+
+
+
+
+
